85d90ec0e168c6e96a27b4079e45599de6e9c3b1
unknown <henrynam28@gmail.com>
2022-01-14 20:22:21 +0700

    Add source

---
+++ djackets_server/product/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import LatestProductsList, ProductDetail, CategoryDetail, search
+
+urlpatterns = [
+    path('latest-products/', LatestProductsList.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/', ProductDetail.as_view()),
+    path('products/<slug:category_slug>/', CategoryDetail.as_view()),
+    path('products/search/', search)
+]
